92307fc4e4d26c5bf3aaa2b0649c90e3a336f03e
rahul@3003 <rahulkhandke71@gmail.com>
2021-09-06 22:32:57 +0530

    2stcommit

--- boxgame.py
+++ boxgame.py
@@ -11,35 +11,40 @@ countright=0
 frame = Frame(root,padx=5,pady=5)
 frame.grid(row=0,column=1,ipadx=10,ipady=5)
 
+my_label=Label(frame)
+
 def moveLabel(countdown,countright):
-	b1=Button(frame, text="M")
-	b1.grid(row=countdown,column=countright)
+	global my_label
+
+	my_label.destroy()
+	my_label=Label(frame, text="M")
+	my_label.grid(row=countdown,column=countright)
 	
 
 def click_down():
 	global clicked,countdown
 
-	if clicked== True :
+	if clicked== True and countdown<num-1:
 		countdown+=1
 		return moveLabel(countdown,countright)
 
 def click_up():
-	global clicked,countup,countdown
+	global clicked,countdown
 
-	if clicked== True :
+	if clicked== True and countdown > 0:
 		countdown-=1
 		return moveLabel(countdown,countright)
 
 def click_right():
 	global clicked,countright
 
-	if clicked== True:
+	if clicked == True and countright<num-1:
 		countright+=1
 		return moveLabel(countdown,countright)
 
 def click_left():
 	global clicked,countright
-	if clicked== True:
+	if clicked == True and countright>0:
 		countright-=1
 		return moveLabel(countdown,countright)
 
@@ -47,7 +52,7 @@ def gridisplay(n):
 	for i in range(n):
 		for j in range(n):
 			if i==0 and j==0:
-				b=Button(frame, text="D",height=3,width=6)
+				b=Button(frame, text="",height=3,width=6)
 				b.grid(row=i,column=j)
 			else:
 				b=Button(frame, text="",height=3,width=6)
@@ -74,9 +79,9 @@ left.grid(row=2,column=0)
 up = Button(frame2, text="up",height=3, width=6, command=click_up)
 up.grid(row=3,column=0)
 
-
+num=6
 moveLabel(countdown, countright)
-gridisplay(4)
+gridisplay(num)
 #displayDevil()
 root.mainloop()
 
